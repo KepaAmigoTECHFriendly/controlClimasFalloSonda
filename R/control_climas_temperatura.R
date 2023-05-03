b8f9d1d131df7f0d77800e9c833c6facc18e7f37
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2023-05-03 12:00:29 +0200

    Add files via upload

--- R/control_climas_temperatura.R
+++ R/control_climas_temperatura.R
@@ -195,7 +195,7 @@ control_climas_temperatura <- function(nombre_PLC, num_climas){
 
 
 
-    if(as.numeric(df_temperatura$temperatura) > df_consignas_seleccion$value[2][[1]] & as.numeric(df_temperatura$temperatura) - df_consignas_seleccion$value[2][[1]] > 0.5){  # HACE CALOR. PUESTA EN MANUAL Y ABRIR FRIO
+    if(as.numeric(df_temperatura$temperatura) > df_consignas_seleccion$value[2][[1]] & as.numeric(df_temperatura$temperatura) - df_consignas_seleccion$value[2][[1]] > 0.1){  # HACE CALOR. PUESTA EN MANUAL Y ABRIR FRIO
 
       # Puesta en manual
       url <- paste("http://88.99.184.239:30951/api/plugins/telemetry/ASSET/",id_planta,"/SERVER_SCOPE",sep = "")
@@ -242,7 +242,7 @@ control_climas_temperatura <- function(nombre_PLC, num_climas){
       )
 
       Sys.sleep(10)
-    }else if(as.numeric(df_temperatura$temperatura) < df_consignas_seleccion$value[1][[1]] & df_consignas_seleccion$value[1][[1]] - as.numeric(df_temperatura$temperatura) > 0.5){  # HACE FRÍO PUESTA EN MANUAL Y ABRIR CALOR
+    }else if(as.numeric(df_temperatura$temperatura) < df_consignas_seleccion$value[1][[1]] & df_consignas_seleccion$value[1][[1]] - as.numeric(df_temperatura$temperatura) > 0.1){  # HACE FRÍO PUESTA EN MANUAL Y ABRIR CALOR
       # Puesta en manual
       url <- paste("http://88.99.184.239:30951/api/plugins/telemetry/ASSET/",id_planta,"/SERVER_SCOPE",sep = "")
       json_envio_plataforma <- paste('{"Modo trabajo climatizadora (auto/man) ',i,'":', '"true"','}',sep = "")
